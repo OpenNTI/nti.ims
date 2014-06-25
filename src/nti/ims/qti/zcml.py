05c1ee18b67a341558136d2131561006a8925293
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-25 03:39:02 +0000

    optimize
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.ims/trunk@42063 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/ims/qti/zcml.py
+++ src/nti/ims/qti/zcml.py
@@ -14,7 +14,6 @@ import inspect
 
 from zope import interface
 from zope.configuration import fields
-from zope.dottedname.resolve import resolve
 
 from nti.externalization.zcml import autoPackageExternalization
 
@@ -25,16 +24,17 @@ from . import interfaces as qti_interfaces
 def _item_predicate(item):
 	implemented = getattr(item, '__implemented__', None)
 	implemented = implemented.flattened() if implemented else ()
-	return  inspect.isclass(item) and issubclass(item, QTIElement) and item != QTIElement and \
-			qti_interfaces.IConcrete in implemented
+	return  inspect.isclass(item) and issubclass(item, QTIElement) and \
+			item != QTIElement and qti_interfaces.IConcrete in implemented
 
 class IRegisterQTIElementsDirective(interface.Interface):
-	module = fields.GlobalObject(title="Module to scan for QTI elements to add", required=True)
+	module = fields.GlobalObject(title="Module to scan for QTI elements to add", 
+								 required=True)
 
 def registerQTIElements(_context, module):
+	root_interfaces = []
 	for name, item in inspect.getmembers(module, _item_predicate):
-		__traceback_info__ = name, item
-		module = resolve(item.__module__)
-		io_interface = list(item.__implemented__.flattened())[0]
-		autoPackageExternalization(_context, (io_interface,), (module,))
-
+		root_iface = list(item.__implemented__.flattened())[0]
+		__traceback_info__ = name, item, root_iface
+		root_interfaces.append(root_iface)
+	autoPackageExternalization(_context, root_interfaces, (module,))
