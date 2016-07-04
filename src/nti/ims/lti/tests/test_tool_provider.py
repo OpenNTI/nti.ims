e4298206bdfabd8768a19ae3c0631e814ed5bdf7
Egawati Panjei <egawati.panjei@nextthought.com>
2016-07-04 08:27:26 +0000

    write the request post values in json for testing purpose
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.ims/branches/ltiproject@91704 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/ims/lti/tests/test_tool_provider.py
+++ src/nti/ims/lti/tests/test_tool_provider.py
@@ -13,10 +13,25 @@ from hamcrest import equal_to
 
 import unittest
 
+import os
+import json
+
+
 from nti.ims.lti.tool_provider import ToolProvider
 
+
+def load_json(fname):
+	path = os.path.join(os.path.dirname(__file__), fname)
+	with open(path, "r") as f:
+		content = f.read()
+		if isinstance(content, bytes):
+			content = unicode(content, 'utf-8')
+	return content
+
 def create_params_moodle():
-	return {'ext_lms': [u'moodle-2'], 'lis_result_sourcedid': [u'{"data":{"instanceid":"2","userid":"2","typeid":null,"launchid":163267342},"hash":"5dc2f4a7d91147c714e79c74fc3c0f40f0b3e79758e36d5a24907a874c8b0d3d"}'], 'context_id': [u'2'], 'tool_consumer_info_version': [u'2016052300.04'], 'tool_consumer_instance_guid': [u'localhost'], 'oauth_signature': [u'm1j3JLAAEC9KysnUrGqIvaCF8KM='], 'context_label': [u'About'], 'lti_message_type': [u'basic-lti-launch-request'], 'ext_user_username': [u'admin'], 'lis_person_name_full': [u'- Admin -'], 'context_title': [u'About Moodle4Mac'], 'user_id': [u'2'], 'tool_consumer_instance_description': [u'Moodle 3.1'], 'oauth_consumer_key': [u'key'], 'launch_presentation_locale': [u'en'], 'lis_outcome_service_url': [u'http://localhost:8888/moodle31/mod/lti/service.php'], 'resource_link_title': [u'NTI_LTI'], 'oauth_callback': [u'about:blank'], 'lis_person_name_family': [u'-'], 'oauth_nonce': [u'80f8a04db6526007ff614c0aeb6f321c'], 'oauth_timestamp': [u'1467096170'], 'oauth_signature_method': [u'HMAC-SHA1'], 'oauth_version': [u'1.0'], 'lis_person_contact_email_primary': [u'admin@localhost'], 'tool_consumer_instance_name': [u'Moodle 3.1'], 'resource_link_id': [u'2'], 'tool_consumer_info_product_family_code': [u'moodle'], 'roles': [u'Instructor,urn:lti:sysrole:ims/lis/Administrator,urn:lti:instrole:ims/lis/Administrator'], 'context_type': [u'CourseSection'], 'lti_version': [u'LTI-1p0'], 'lis_person_name_given': [u'- Admin'], 'launch_presentation_return_url': [u'http://localhost:8888/moodle31/mod/lti/return.php?course=2&launch_container=3&instanceid=2&sesskey=ZLRo9M4mJd'], 'launch_presentation_document_target': [u'iframe']}
+	content = load_json('moodle_request_values_sample.json')
+	params = json.loads(content)
+	return params
 
 class TestToolProvider(unittest.TestCase):
 
@@ -65,10 +80,8 @@ class TestToolProvider(unittest.TestCase):
 		assert_that(hasattr(TP, 'launch_presentation_document_target'), equal_to(True))
 		
 		assert_that(hasattr(TP, 'ext_params'), equal_to(True))
-		#assert_that(hasattr(TP, 'ext_user_username'), equal_to(True))
-		#assert_that(hasattr(TP, 'userid'), equal_to(True))
-		#assert_that(hasattr(TP, 'ext_lms'), equal_to(True))
-
+		
+		assert_that(TP.oauth_consumer_key, equal_to(consumer_key))
 
 	def test_generated_outcome_service_request_for_moodle(self):
 		params = create_params_moodle()
@@ -77,7 +90,7 @@ class TestToolProvider(unittest.TestCase):
 		TP = ToolProvider(consumer_key, consumer_secret, params)
 
 		assert_that(TP.lis_outcome_service_url, equal_to(u'http://localhost:8888/moodle31/mod/lti/service.php'))
-		assert_that(TP.lis_result_sourcedid, equal_to(u'{"data":{"instanceid":"2","userid":"2","typeid":null,"launchid":163267342},"hash":"5dc2f4a7d91147c714e79c74fc3c0f40f0b3e79758e36d5a24907a874c8b0d3d"}'))
+		assert_that(TP.lis_result_sourcedid, equal_to(u'{"data":{"instanceid":"2","userid":"2","typeid":null,"launchid":1559272422},"hash":"cab2467ffa02f501386f24f3dcbe71999d30fde4ebc707afb4962152c432394a"}'))
 
 		outcome_request_xml = TP.generate_outcome_request_xml()
 		print(outcome_request_xml)
