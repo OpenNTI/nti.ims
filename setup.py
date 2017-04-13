7f2c4785be76b4fb090d0fd1133b2df396cc8ec5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-13 14:12:53 +0000

    peo8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.ims/trunk@110659 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -16,43 +16,43 @@ TESTS_REQUIRE = [
 ]
 
 setup(
-	name='nti.ims',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI IMS Integration",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='IMS, LTI, QTI',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: Implementation :: CPython'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	tests_require=TESTS_REQUIRE,
-	install_requires=[
-		'setuptools',
-		'dolmen.builtins',
-		'lxml',
-		'ims_lti_py',
-		'nti.externalization',
-		'nti.property',
-		'nti.schema',
+    name='nti.ims',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI IMS Integration",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='IMS, LTI, QTI',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'dolmen.builtins',
+        'lxml',
+        'ims_lti_py',
+        'nti.externalization',
+        'nti.property',
+        'nti.schema',
         'zope.component',
         'zope.container',
         'zope.interface',
         'zope.proxy',
         'zope.schema',
         'zope.security',
-	],
-	extras_require={
-		'test': TESTS_REQUIRE,
-	},
-	entry_points=entry_points
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points
 )
