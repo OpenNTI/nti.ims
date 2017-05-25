19315638cdc007f420efafe4cbc938735659aa1d
Carlos Sanchez <papachoco@gmail.com>
2017-05-25 10:27:00 -0500

    add supporting files

---
+++ README.rst
@@ -0,0 +1,9 @@
+=========
+ nti.ims
+=========
+
+.. image:: https://travis-ci.org/NextThought/nti.ims.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.ims
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.ims/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.ims?branch=master
--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
-
 entry_points = {
+    'console_scripts': [
+    ],
 }
 
 TESTS_REQUIRE = [
-    'fudge',
     'nti.testing',
     'zope.testrunner',
 ]
 
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
 setup(
     name='nti.ims',
-    version=VERSION,
+    version=_read('version.txt').strip(),
     author='Jason Madden',
     author_email='jason@nextthought.com',
-    description="NTI IMS Integration",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='IMS, LTI, QTI',
+    description="NTI IMS",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='IMS LTI',
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
@@ -29,8 +33,10 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython'
     ],
+    zip_safe=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     namespace_packages=['nti'],
     tests_require=TESTS_REQUIRE,
     install_requires=[
@@ -50,5 +56,6 @@ setup(
     extras_require={
         'test': TESTS_REQUIRE,
     },
-    entry_points=entry_points
+    entry_points=entry_points,
+    test_suite="nti.ims.tests",
 )
