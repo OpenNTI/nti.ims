a636adbfb869ea3dc10f469521d6c9fd4f3fd75f
Sheldon Smith <sheldon.smith@nextthought.com>
2017-07-20 13:29:42 -0500

    Modified utils to work for single and multi adapter filtering

--- src/nti/ims/lti/utils.py
+++ src/nti/ims/lti/utils.py
@@ -8,6 +8,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope.component import queryAdapter
+from zope.component import queryMultiAdapter
 
 DEFAULT_FIELDS = [
     'tool_consumer_instance_guid',
@@ -18,16 +19,17 @@ DEFAULT_FIELDS = [
 ]
 
 
-class LaunchRequestFilter(object):
+# TODO these methods can be combined
+class LTIRequestFilter(object):
 
     @classmethod
-    def filter_for_adapter(cls, request, interface, fields=DEFAULT_FIELDS):
+    def user_adapter_filter(cls, request, user_interface, fields=DEFAULT_FIELDS):
         """
         Filters a launch request specific fields
         :param request: ILTIRequest
-        :param interface: the interface being adapted
-        :param fields: where to check for the value
-        :return: the field name where the value is contained, or None if not found
+        :param user_interface: the interface being adapted
+        :param fields: where to check for the adapter
+        :return: adapter, or None if not found
         """
 
         params = request.params
@@ -37,8 +39,35 @@ class LaunchRequestFilter(object):
             try:
                 adapter_name = params[field]
                 adapter = queryAdapter(request,
-                                       interface,
+                                       user_interface,
                                        name=adapter_name)
+                if adapter:
+                    return adapter
+            except KeyError:
+                logger.exception('No key in field %s', field)
+
+        return None
+
+    @classmethod
+    def mapping_adapter_filter(cls, request, factory, map_interface, fields=DEFAULT_FIELDS):
+        """
+        Filters a launch request specific fields
+        :param request: ILTIRequest
+        :param factory: package specific factory
+        :param map_interface: the interface being adapted
+        :param fields: where to check for the adapter
+        :return: adapter, or None if not found
+        """
+
+        params = request.params
+
+        # Check the suspected locations
+        for field in fields:
+            try:
+                adapter_name = params[field]
+                adapter = queryMultiAdapter((request, factory),
+                                            map_interface,
+                                            name=adapter_name)
                 if adapter:
                     return adapter
             except KeyError:
