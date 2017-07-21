82663e557fd53a8382897656b121c1292bcdcecb
Sheldon Smith <sheldon.smith@nextthought.com>
2017-07-21 09:54:53 -0500

    Refactored LTIRequestFilter into adapt_accounting_for_consumer and moved to init.py

--- src/nti/ims/lti/__init__.py
+++ src/nti/ims/lti/__init__.py
@@ -8,3 +8,45 @@ from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+
+from zope.component import getAdapter
+from zope.component import getMultiAdapter
+from zope.component import queryAdapter
+from zope.component import queryMultiAdapter
+
+DEFAULT_FIELDS = [
+    'tool_consumer_instance_guid',
+    'tool_consumer_instance_url',
+    'tool_consumer_instance_name',
+    'oauth_consumer_key',
+    'tool_consumer_info_product_family_code'
+]
+
+
+def adapt_accounting_for_consumer(request,
+                                  interface,
+                                  multi=False,
+                                  query=True,
+                                  fields=DEFAULT_FIELDS):
+
+    params = request.params
+
+    foo = queryAdapter if query else getAdapter
+
+    if isinstance(request, tuple) or multi:
+        foo = queryMultiAdapter if query else getMultiAdapter
+
+    # Check the suspected locations
+    for field in fields:
+        try:
+            adapter_name = params[field]
+            adapter = foo(request,
+                          interface,
+                          name=adapter_name)
+            if adapter:
+                return adapter
+        except KeyError:
+            logger.exception('No key in field %s', field)
+
+    return None
