e3488b9702e9186349423e929139e3bec1d1e92c
Sheldon Smith <sheldon.smith@nextthought.com>
2017-07-25 09:01:07 -0500

    adapt_accounting_for_consumer revisions

--- src/nti/ims/lti/__init__.py
+++ src/nti/ims/lti/__init__.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-.. $Id$
-"""
 
 from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-
 from zope.component import getAdapter
 from zope.component import getMultiAdapter
 from zope.component import queryAdapter
 from zope.component import queryMultiAdapter
-
-from nti.ims import MessageFactory as _
 
 DEFAULT_FIELDS = [
     'tool_consumer_instance_guid',
@@ -29,25 +23,25 @@ DEFAULT_FIELDS = [
 def adapt_accounting_for_consumer(request,
                                   adaptee,
                                   interface,
-                                  multi=False,
+                                  multi_adapter_for_tuple=True,
                                   query=True,
                                   fields=DEFAULT_FIELDS):
 
     params = request.params
 
     query_type = queryAdapter if query else getAdapter
-    if isinstance(request, tuple) or multi:
+    if isinstance(adaptee, tuple) and multi_adapter_for_tuple:
         query_type = queryMultiAdapter if query else getMultiAdapter
 
     for field in fields:
         try:
             field_value = params[field]
-            adapter = query_type(adaptee, interface, field_value)
+            adapter = query_type(adaptee, interface, name=field_value)
             if adapter is not None:
                 return adapter
         except KeyError:
-            msg = _('No value for field %s', field)
-            logger.exception(msg)
+            # This will likely occur often, and malformed requests are handled elsewhere
+            pass
 
-    return None
+    return query_type(adaptee, interface)
 
