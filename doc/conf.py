a39dba6e74e87b7daddc78ef6dc5b04548bb04b7
Carlos Sanchez <papachoco@gmail.com>
2017-09-07 20:51:14 -0500

    fix var name

--- doc/conf.py
+++ doc/conf.py
@@ -105,8 +105,8 @@ html_static_path = ['_static']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
-# Output file ims name for HTML help builder.
-htmlhelp_imsname = 'ntiimsdoc'
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'ntiimsdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
