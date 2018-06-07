98f6d0e6302a8feaf53fe904ea7a464bb1a13fe4
Sheldon Smith <sheldon.smith@nextthought.com>
2018-06-07 15:16:13 -0500

    Provide IDeletedObjectPlaceholder on import if needed

--- src/nti/ims/lti/internalization.py
+++ src/nti/ims/lti/internalization.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, absolute_import, division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from nti.dataserver.interfaces import IDeletedObjectPlaceholder
 
 from nti.externalization.datastructures import InterfaceObjectIO
 
@@ -20,9 +24,12 @@ class _ConfiguredToolImportUpdater(InterfaceObjectIO):
 
     def updateFromExternalObject(self, parsed, *args, **kwargs):
         if 'config_xml' in parsed:  # This is the import case
+            from IPython.core.debugger import Tracer;Tracer()()
             config = PersistentToolConfig.create_from_xml(parsed['config_xml'])
             ext_self = self._ext_replacement()
             self._ext_setattr(ext_self, 'config', config)
             self._excluded_in_ivars_ = 'config'
+            if parsed['deleted']:
+                ext_self.alsoProvides(IDeletedObjectPlaceholder)
         result = super(_ConfiguredToolImportUpdater, self).updateFromExternalObject(parsed, *args, **kwargs)
         return result
