7cf1bc95a0938dc15b46d8cae9a15f5019032ba0
Sheldon Smith <sheldon.smith@nextthought.com>
2018-07-18 14:52:47 -0500

    Find the start of the xml string

--- src/nti/ims/lti/consumer.py
+++ src/nti/ims/lti/consumer.py
@@ -232,6 +232,7 @@ class PersistentToolConfig(ToolConfig, PersistentCreatedAndModifiedTimeObject):
     def create_from_xml(xml):
         config = PersistentToolConfig()
         xml = xml.encode('utf-8')  # This must be a string, unicode will not work
+        xml = xml[xml.find('<'):] # Make sure we start with the opening tag
         config.process_xml(xml)
         return config
 
