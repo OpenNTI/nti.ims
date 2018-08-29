69c9d125286fb31c09db4a60da026d0f64d5eabc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-29 14:52:14 -0500

    use frozenset; pylint

--- src/nti/ims/lti/internalization.py
+++ src/nti/ims/lti/internalization.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
+from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import division
 
 from nti.base._compat import bytes_
 
@@ -12,8 +15,6 @@ from nti.externalization.datastructures import InterfaceObjectIO
 from nti.ims.lti.consumer import PersistentToolConfig
 
 from nti.ims.lti.interfaces import IConfiguredTool
-
-__docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
@@ -25,7 +26,7 @@ class _ConfiguredToolImportUpdater(InterfaceObjectIO):
     # We are creating these from an import and we want to preserve
     # their old id so we can find them later on in the container
     # to update the ExternalToolAsset
-    def _ext_accept_external_id(self, ext_self, parsed):
+    def _ext_accept_external_id(self, unused_ext_self, parsed):
         if 'config_xml' in parsed:  # This is the import case
             return True
         return False
@@ -35,7 +36,7 @@ class _ConfiguredToolImportUpdater(InterfaceObjectIO):
             config = PersistentToolConfig.create_from_xml(bytes_(parsed['config_xml']))
             ext_self = self._ext_replacement()
             self._ext_setattr(ext_self, 'config', config)
-            self._excluded_in_ivars_ = 'config'
+            self._excluded_in_ivars_ = frozenset(('config',))
             # We previously checked to see if this tool was deleted here, but we have moved that
             # up into the container internalizer so that these objects are fully created when
             # we decorate conditional interfaces
